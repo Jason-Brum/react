9a6cb63956834cbd56764c90de553d4cbce6e5f3
Jason-Brum <contato@jasonb.com.br>
2025-01-18 13:05:01 -0300

    ok

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+//styles/css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Teste Fundamentos com React</h1>
+     <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
