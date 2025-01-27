fe4683444967464f983b9b5937ae0ee3810dd36c
Jason-Brum <contato@jasonb.com.br>
2025-01-27 17:48:49 -0300

    ok

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -3,12 +3,23 @@ import {useState} from "react";
 const ListRender = () => {
     const [list] = useState(["Bola", "Raquete", "Tênis", "Bastão"]);
 
-    const[products] = useState ([
+    const[products, setProducts] = useState ([
         {id: 1, model: "Run", size: 41},
         {id: 2, model: "Tracker", size: 40},
         {id: 3, model: "Fast", size: 39},
 
     ]);
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevProducts) => {
+
+            return prevProducts.filter((product) => randomNumber !== product.id)
+
+        })
+    }
+
     return (
         <div>
             <ul>
@@ -24,6 +35,7 @@ const ListRender = () => {
 
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random product</button>
         </div>
     );
 }
