4c8eb174d702c82178c8b71e1cd9f2f71d4cc35c
Jason-Brum <contato@jasonb.com.br>
2025-01-28 17:40:19 -0300

    Ok

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -4,8 +4,12 @@ import City from "./assets/city.jpg"
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+const name = "Zé"
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -20,6 +24,11 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/*Props*/}
+      <ShowUserName name={name}/>
+      {/*Destructuring*/}
+      <CarDetails brand="Ford" km={2.200} color="Prata"/>
+
     </div>
   );
 }
--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -4,11 +4,19 @@ import { useState } from "react"
 function ConditionalRender() {
     const [x] = useState (false);
 
+    const [name] = useState("João")
+
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim! </p> }
         {!x && <p>Agora X é falso </p> }
+        {name === "João" ? (
+            <div> <p>O nome é João</p>
+            </div>
+        ) : (
+            <div> <p>Nome não encontrado</p> </div>
+        )}
 
     </div>
   )
