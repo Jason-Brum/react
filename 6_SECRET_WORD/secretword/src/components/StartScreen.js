3f55de7f711ecd6bbddda252863b65964bf86585
Jason-Brum <contato@jasonb.com.br>
2025-02-20 09:25:35 -0300

    ok

--- 6_SECRET_WORD/secretword/src/components/StartScreen.js
+++ 6_SECRET_WORD/secretword/src/components/StartScreen.js
@@ -5,9 +5,9 @@ const StartScreen = ({startGame}) => {
     return  <div className="start">
         <h1>Roletrando</h1>
         <p>Clique no botão abaixo para começar a jogar</p>
-        <button onClick = {startGame} className="start-button">
-            Começar o jogo
-        </button>
+        <button onClick = {startGame}>Começar o jogo</button> 
+            
+        
     </div>;
 }
 
